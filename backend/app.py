284a0e897fbbe2634b9b3d4179129d10a33c7a7f
hadiyafarooq <113384011+hadiyafarooq@users.noreply.github.com>
2023-12-12 22:29:09 +0500

    Working API

--- backend/app.py
+++ backend/app.py
@@ -10,20 +10,23 @@ supabase_url = "https://ihfogxstivbgpxjxrxex.supabase.co"
 supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImloZm9neHN0aXZiZ3B4anhyeGV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIzMzMwNzUsImV4cCI6MjAxNzkwOTA3NX0.7kgGD988sh__a9UHNAFL3UIpsBaPpsbXF9tvtbvUepU"
 supabase_client = supabase.Client(supabase_url, supabase_key)
 
+
 @app.route('/Login', methods=['POST'])
 def login():
     try:
-        # Replace 'your_table' with the actual table name
-        table_name = "Users"
-
-        # Access email and password from the request data
         request_data = request.get_json()
-        username = request_data.get('username', '')
-        password = request_data.get('password', '')
+        # Now request_data contains the JSON data sent in the request body
+        # You can access username and password like this:
+        username = request_data.get('username')
+        password = request_data.get('password')
 
+        # Your authentication logic goes here
         # Perform a SELECT query to get all rows from the specified table
-        response = supabase_client.table(table_name).select('username', 'password').execute()
-        data = response['data', []]
+        response = supabase_client.table('Users').select(
+            'username', 'password').execute()
+        response_data_str = response.json()
+        response_data = json.loads(response_data_str)
+        data = response_data.get('data', [])
 
         # Perform authentication logic here
         if any(entry['username'] == username and entry['password'] == password for entry in data):
@@ -38,5 +41,6 @@ def login():
         traceback.print_exc()
         return jsonify({'success': False, 'error': str(e)}), 500
 
+
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(port=5001, debug=True)
